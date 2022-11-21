eb13d507de8d0fc79dfe4076952a85d4517e20d2
furkanunutmaz <unutmazfurkan@gmailgit config --global user.name furkanunutmazgit commit -m First>
2022-11-22 00:19:43 +0300

    try to delete_item

--- js/js.js
+++ js/js.js
@@ -1,13 +1,12 @@
 /*
 
 1. Adım
-    -> buton aktifleştirilecek
-    -> array içerisinde loop yapacak
+    -> buton aktifleştirilecek -> Tamamlandı
     -> DOM işlemi uygulanacak
     -> localstorage'da veri tutucak
 
 2. Adım 
-    -> Listeye boş karakter eklenemeyecek
+    -> Listeye boş karakter eklenemeyecek -> Tamamlandı
     -> Tamamlandı işareti koyulabilecek
     -> çarpıyla silme işlemi koyulabilecek
     -> Toast(bootstrap) ile hata verecek
@@ -36,11 +35,14 @@ for (index = 0; index < item.length; index++){
 // Buton, text ve liste tanımlandı ardında text alanından girilen değer buton yardımıyla listeye eklendi.
 // Daha düzgün yapılacak
 
+
 let task = document.getElementById('task')
 let get_task = document.getElementById('list')
-const add_button = document.getElementById('liveToastBtn')
+let add_button = document.getElementById('liveToastBtn')
+let delete_button = document.getElementById('iconForDelete')
 
 add_button.addEventListener('click', add_task)
+get_task.addEventListener('click', delete_task)
 
 function add_task (){
 
@@ -51,14 +53,16 @@ function add_task (){
     else {
         let li = document.createElement('li')
 
-        li.innerText = task.value
+        // örnek kullanım  <input id="${todo.id}" type="checkbox"/> <label for="${todo.id}" class="tick js-tick"></label> <span>${todo.text}</span>  <button class="delete-todo js-delete-todo">  <svg><use href="#delete-icon"></use></svg> </button>
+        li.innerHTML = `${task.value}  <button id="iconForDelete" class="btn btn-outline-secondary"><i class="bi bi-x-circle-fill"></i></button>`
     
         // i elementini seçerek i değişkenine ata
-        var i = document.createElement("i");
+        let i = document.createElement("i");
             
         // i değişkenine class ataması yap
         i.classList = "fas fa-times";
-            
+     
+        // Appen <button id="iconForDelete" class="btn btn-outline-secondary"><i class="bi bi-x-circle-fill"></i></button>
         // i değişkenini li değişkenine ekleme
         li.appendChild(i);
      
@@ -67,3 +71,20 @@ function add_task (){
     }
 
 }
+
+// DELETE FUNCTION
+/*
+
+            // Declaring a variable to get select element
+            var a = document.getElementById("list");
+            var candidate = document.getElementById("candidate");
+            var item = document.getElementById(candidate.value);
+            a.removeChild(item);
+*/
+
+function delete_task(){
+
+    let delete_item = document.getElementById(delete_button.value)
+    delete_button.removeChild(delete_item)
+}
+
